2105183b663b3cabb4031e888d35445601a12bd2
NateChoe1 <thetinybros@gmail.com>
2020-12-29 03:15:35 -0600

    Added the GPL v3

--- paused.js
+++ paused.js
@@ -1,3 +1,27 @@
+/*
+    Nate's accuracy trainer, it's a mouse accuracy trainer.
+    Copyright (C) 2020  Nathaniel Choe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 function pausedDraw() {
-  displayMessage("The game is paused. Press escape to continue.\nYou are playing beta version 1.0.0");
+  displayMessage("The game is paused. Press escape to continue.\n" +
+                 "You are playing beta version 1.0.0\n\n" +
+
+                 "Nate's accuracy trainer  Copyright (C) 2020  Nathaniel Choe.\n" +
+                 "This program comes with ABSOLUTELY NO WARRANTY.\n" +
+                 "This is free softwarem and you are welcome to to redistribute it\n"+
+                 "under certain conditions; see license.txt for details.");
 }//For consistency and also to standardize.
--- universal.js
+++ universal.js
@@ -1,3 +1,21 @@
+/*
+    Accuracy, it's a mouse accuracy trainer.
+    Copyright (C) 2020  Nathaniel Choe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 let mode = 1;
 let selectedMode = 1;
 let playedFrames = 0;//This is constantly increasing and will only be reset on new games. There will be no other instances in which this value changes. Do not create any, do not delete any. These are constant. I repeat, do not change playedFrames, only read from it.
